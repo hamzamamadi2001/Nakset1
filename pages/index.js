699ae113d47415f16830ed49c34c0bccfc13430c
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2022-11-28 12:25:50 +0100

    new version

--- pages/index.js
+++ pages/index.js
@@ -171,7 +171,7 @@ className='text-lg sm:text-4xl font-rubik '
 }
 export async function getServerSideProps(context) {
   
-    let response = await fetch("https://nakset-nargwu9lm-hamzamamadi2001.vercel.app/api/register")
+    let response = await fetch("https://nakset.vercel.app/api/listCategories")
 
 let result = await response.json()
  console.log(result)
