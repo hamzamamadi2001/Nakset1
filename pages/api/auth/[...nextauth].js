4c6cbee96b32f18c229aeb08d6ead9d5aa0a9880
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2022-12-21 19:25:16 +0100

    anothr version

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -32,7 +32,9 @@ export default NextAuth({
    
       }
         let result =  await   prisma.user.findUnique({ where: {
-        email: user.email?user.email:uuimage
+        email: user.email?user.email:uuimage,
+        provider:account.provider
+
       },})
        
       
@@ -48,7 +50,7 @@ export default NextAuth({
           }
             
       }else{
-        console.log("this is the user opject",user.email)
+       
         let newuser =  await   prisma.user.create({ data: {
           email:user.email?user.email:uuimage,
           name:user.name,
