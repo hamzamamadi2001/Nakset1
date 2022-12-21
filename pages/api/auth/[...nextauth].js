b60704fb9cbc835939237274cfc1e9aec8b55332
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2022-12-21 15:32:26 +0100

    anothr version

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -46,7 +46,7 @@ export default NextAuth({
       }else{
         console.log("this is the user opject",user.email)
         let newuser =  await   prisma.user.create({ data: {
-          email:user.email,
+          email:user.email?user.email:"no email available",
           name:user.name,
           password:'',
  provider :account.provider,
