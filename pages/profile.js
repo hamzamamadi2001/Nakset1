a855d732e6364d9dd9e746b8632aaf5fea5cb2e7
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2022-12-26 11:19:30 +0100

    anothr version

--- pages/profile.js
+++ pages/profile.js
@@ -1,29 +1,36 @@
 import React from 'react'
 import {useSession,getSession} from 'next-auth/react'
-
+import Image from'next/image'
 
 function About() {
+  const {data:session}= useSession()
+  console.log("this is the session",session)
   return (
-    <h1>profile</h1>
+    <div className="container mx-auto bg-slate-600 w-full h-screen">
+      {/* <div className='w-full flex justify-center items-center'><Image src={sess}></Image></div> */}
+
+
+
+    </div>
   )
 }
-export async function getServerSideProps({req}){
-  const session = await getSession({req})
+// export async function getServerSideProps({req}){
+//   const session = await getSession({req})
   
 
-  if(!session){ return{
-    redirect: {
-      permanent: false,
-      destination: '/sum',
+//   if(!session){ return{
+//     redirect: {
+//       permanent: false,
+//       destination: '/sum',
 
-  }}
+//   }}
   
-}else{
-  return {
-    props: {
-      session
-    }
-  }
-}
-}
+// }else{
+//   return {
+//     props: {
+//       session
+//     }
+//   }
+// }
+// }
 export default About
