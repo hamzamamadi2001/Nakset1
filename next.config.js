e098bc55772d9f448f52096dc8e19060e6f67be8
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-01-27 17:33:56 +0100

    new update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,54 @@
 const nextTranslate = require('next-translate')
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   optimizeFonts: true,
+   
+  
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "res.cloudinary.com",
+//       },
+//     ],
+//     minimumCacheTTL: 1500000,
+//   },
+// };
+// module.exports = nextTranslate({
+//   webpack: (nextConfig, { isServer, webpack }) => {
+//     if (!isServer) {
+//       nextConfig.resolve.fallback.fs = false;
+//     }
+//     return nextConfig
+//   }
+   
+  
+  
+// })
+
+// webpack: (config, { isServer }) => {
+//   if (!isServer) {
+//     config.resolve.fallback.fs = false;
+//   }
+//   return config;
+// },
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  optimizeFonts: true,
-   
+
+  webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.resolve.fallback =  {fs: false,
+        path: false,
+        child_process: false,
+      };
+      }
+      return config;
+    },
   
+}
+module.exports = {
   images: {
     remotePatterns: [
       {
@@ -14,24 +58,6 @@ const nextConfig = {
     ],
     minimumCacheTTL: 1500000,
   },
-};
-module.exports = nextTranslate({
-  webpack: (config, { isServer, webpack }) => {
-    if (!isServer) {
-      nextConfig.node = {
-        fs: 'empty'
-      }
-    }
-    return nextConfig
-  }
-   
   
-  
-})
-//  module.exports = nextConfig;
-// webpack: (config, { isServer }) => {
-//   if (!isServer) {
-//     config.resolve.fallback.fs = false;
-//   }
-//   return config;
-// },
+}
+module.exports = nextConfig
