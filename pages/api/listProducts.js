f1e4ae879fd380cc2126d26d4f09b0362a5b6591
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-01-27 19:26:45 +0100

    new update

--- pages/api/listProducts.js
+++ pages/api/listProducts.js
@@ -5,15 +5,15 @@ const prisma = new PrismaClient()
    
  
 
-   const categories = await prisma.category.findMany({select:{id:true,name:true}}).finally(async()=>{prisma.$disconnect()})
-   const countrys = await prisma.countrys.findMany().finally(async()=>{prisma.$disconnect()})
-   const citys = await prisma.citys.findMany({where:{country:countrys[0].id},select:{id:true,name:true}}).finally(async()=>{prisma.$disconnect()})
+  //  const categories = await prisma.category.findMany({select:{id:true,name:true}}).finally(async()=>{prisma.$disconnect()})
+  //  const countrys = await prisma.countrys.findMany().finally(async()=>{prisma.$disconnect()})
+  //  const citys = await prisma.citys.findMany({where:{country:countrys[0].id},select:{id:true,name:true}}).finally(async()=>{prisma.$disconnect()})
 
 
   const products = await prisma.product.findMany({where:{city:citys[0].id}}).finally(async()=>{prisma.$disconnect()})
   
  
-     return res.status(200).json({data:[categories,countrys,citys,products]})              
+     return res.status(200).json({data: products })              
 
   
   
