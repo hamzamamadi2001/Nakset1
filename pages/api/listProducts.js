c34eb395ecd4defeddd66c0980d064994238a83a
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-01-28 01:46:27 +0100

    new update

--- pages/api/listProducts.js
+++ pages/api/listProducts.js
@@ -6,13 +6,11 @@
 
    const categories = await client.category.findMany({select:{id:true,name:true}}) 
     
-   const citys = await client.citys.findMany({where:{country:1},select:{id:true,name:true}}) 
-
-
-  const products = await client.product.findMany({where:{city:citys[0].id}})
-  
  
-     return res.status(200).json({data:[categories,citys,products]})              
+
+   
+ 
+     return res.status(200).json( [categories])              
 
   
   
