39f687375e8e8b5438b36c2c72b9a8f1968474d0
Hamza-Algerian- <103936497+hamzamamadi2001@users.noreply.github.com>
2023-03-30 13:49:24 +0200

    latest upadates

--- pages/nakset.js
+++ pages/nakset.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from "next/image"
 import Button from '@mui/material/Button';
 import ReactPlayer from 'react-player'
-import Link from 'next/Link';
+import Link from 'next/link';
 
 function About() {
   return (
